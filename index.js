ae17b6f7f2af467b0b1b3d518c783e6f98e1eef1
CyberZHG <CyberZHG@gmail.com>
2016-08-26 09:43:16 +0800

    Dynamically.
    Signed-off-by: CyberZHG <CyberZHG@gmail.com>

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
+/*jslint node*/
+
 module.exports = {
-    website: {
-        html: {
-            "head:start": function () {
-                'use strict';
-                var data = this.options.pluginsConfig.meta.data || [],
-                    heads = '<meta name="gitbook-plugin-meta" content="https://github.com/CyberZHG/gitbook-plugin-meta">';
-                data.forEach(function (datum) {
-                    heads += '<meta';
-                    for (var name in datum) {
-                        if (!datum.hasOwnProperty(name)) {
-                            continue;
-                        }
-                        var value = datum[name];
-                        heads += ' ' + name + '="' + value + '""';
-                    }
-                    heads += '>';
-                });
-                return heads;
-            }
-        }
+    book: {
+        assets: "./book",
+        js: ["plugin.js"]
     }
 };
